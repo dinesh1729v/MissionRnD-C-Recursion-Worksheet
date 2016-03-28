71a0d6efdd1e24c86f3346d83d9172de77a711ad
dinesh1729v <dinesh1729v@missionrnd.com>
2016-03-28 16:52:39 +0530

    Submit Commit

--- src/MainRecursion.cpp
+++ src/MainRecursion.cpp
@@ -1,13 +1,13 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <stdlib.h>
-
+#include<stdio.h>
 #include "FunctionHeaders.h"
 
 int main(){
-	
+
 	//Check NSnipers
 
-	//Check CountPairs Sample Code 
+	//Check CountPairs Sample Code
 
 	//Check Steps Sample Code
 
@@ -15,7 +15,7 @@ int main(){
 
 	//Check Maze Sample Code
 
-	/*
+
 	int maze[4][3] = {
 		{ 0, 0, 1 },
 		{ 1, 1, 1 },
@@ -28,8 +28,8 @@ int main(){
 
 	int check=path_exists(&maze[0][0], m, n, x1, y1, x2, y2);
 	printf("%d\n", check);
-	*/
 
-	
+
+
 	return 0;
 }
--- src/Maze.cpp
+++ src/Maze.cpp
@@ -16,8 +16,8 @@ Maze of order 4x4:
 In this case, there exists a connected path:
 1
 1	1
-	1		1
-	1	1	1
+1		1
+1	1	1
 
 Since there is a connected path, your function has to return 1.
 If a path doesn't exist between two co-ordinates then return 0.
@@ -34,9 +34,41 @@ more parameters .
 */
 
 #include<stdlib.h>
-
+int canProceed(int *maze, int rows, int columns, int x1, int y1, int **alreadyVisited)
+{
+	if (x1 >= 0 && x1 < rows&&y1 >= 0 && y1 < columns && (*((maze + x1*columns) + y1) == 1) && alreadyVisited[x1][y1] == 0)return 1;
+	return 0;
+}
+int pathChecker(int *maze, int rows, int columns, int x1, int y1, int x2, int y2, int **alreadyVisited)
+{
+	if (x1 == x2 && y1 == y2)
+	{
+		alreadyVisited[x1][y1] = 1;
+		return 1;
+	}
+	if (canProceed(maze, rows, columns, x1, y1, alreadyVisited))
+	{
+		alreadyVisited[x1][y1] = 1;
+		if (pathChecker(maze, rows, columns, x1 + 1, y1, x2, y2, alreadyVisited))return 1;
+		if (pathChecker(maze, rows, columns, x1, y1 - 1, x2, y2, alreadyVisited))return 1;
+		if (pathChecker(maze, rows, columns, x1, y1 + 1, x2, y2, alreadyVisited))return 1;
+		if (pathChecker(maze, rows, columns, x1 - 1, y1, x2, y2, alreadyVisited))return 1;
+		return 0;
+	}
+	return 0;
+}
 
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	return 1;
+	if (*((maze + x1*columns) + y1) == 0 || *((maze + x2*columns) + y2) == 0)return 0;
+	if (maze == NULL)return NULL;
+	if ((rows <= 0 || columns <= 0) || x1<0 || y1<0 || x2<0 || y2<0)return 0;
+	if (x1>x2)//swapping...since checking for path from A->B is equal to checking for path from B->A..
+	{
+		x1 = x1 + x2 - (x2 = x1);
+		y1 = y1 + y2 - (y2 = y1);
+	}
+	int **alreadyVisited = (int **)calloc(rows, sizeof(int *));
+	for (int i = 0; i < rows; i++)alreadyVisited[i] = (int *)calloc(columns, sizeof(int));
+	return pathChecker((int *)maze, rows, columns, x1, y1, x2, y2, alreadyVisited);
 }
